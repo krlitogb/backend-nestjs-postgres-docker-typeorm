5db5919b3ead4ba800df6f32c7ee5192bbb7aa11
Carlos Gimenez <krlito.gb@gmail.com>
2025-02-18 17:07:07 -0300

    Add SSL

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,29 +6,35 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 
-
-
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
 
-   ConfigModule.forRoot({
-    isGlobal: true,
-   }),
-   
-   TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: process.env.POSTGRES_HOST,
-    port: Number(process.env.POSTGRES_PORT),
-    username: process.env.POSTGRES_USERNAME,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DATABASE,
-    autoLoadEntities: true,
-    synchronize: true,
-  }),
-   CatsModule,
-   BreedsModule,
-   UsersModule,
-   AuthModule
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USERNAME,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+      ssl: process.env.POSTGRES_SSL === 'true',
+      extra: {
+        ssl:
+          process.env.POSTGRES_SSL === 'true'
+            ? {
+                rejectUnauthorized: false,
+              }
+            : null,
+      },
+    }),
+    CatsModule,
+    BreedsModule,
+    UsersModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
